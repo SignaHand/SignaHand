70349dedafade02edccc38fd79ee43919c1d330b
codeartitect <ysw991106@gmail.com>
2023-08-18 00:58:17 +0900

    fix: 한 페이지씩 보여주기 완료, preview와 연동 고려, 서명 이미지 붙이기 기능 미완

--- src/page/edit/layout/pdfDisplay/PdfDisplay.tsx
+++ src/page/edit/layout/pdfDisplay/PdfDisplay.tsx
@@ -6,6 +6,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as pdfjsLib from "pdfjs-dist";
 import Loading from "../loading/Loading";
+import {PageViewport, PDFPageProxy} from "pdfjs-dist";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`;
 
@@ -13,70 +14,93 @@ interface PdfDisplayProps {
     file: File;
 }
 
-const PdfDisplay: React.FC<PdfDisplayProps> = ({ file }) => {
+const PdfDisplay: React.FC<PdfDisplayProps> = ({file}) => {
     const containerRef = useRef<HTMLDivElement>(null);
     const [loading, setLoading] = useState(true);
+    const [numPages, setNumPages] = useState<number>(100);
+    const [currentPage, setCurrentPage] = useState<number>(1);
 
     /* PDF 파일을 뷰어에 띄우기 위해서는 URL을 사용해야함 */
     const pdfUrl = URL.createObjectURL(file);
+    /* PDF 문서 업로딩 */
+    const loadingTask = pdfjsLib.getDocument(pdfUrl);
+    const temp = "container";
 
     useEffect(() => {
         /* PDF 불러오기 */
         const loadPDF = async () => {
-            /* pdf를 보여줄 container Element 불러오기 */
-            const container = containerRef.current;
-            if (!container) return; // 컨테이너가 없다면 종료
-
+            /* PDF 문서 객체 */
+            const pdf = await loadingTask.promise;
+            setNumPages(pdf.numPages);
+            const page = await pdf.getPage(currentPage);
             try {
-                /* PDF 문서 업로딩 */
-                const loadingTask = pdfjsLib.getDocument(pdfUrl);
-                /* PDF 문서 객체 */
-                const pdf = await loadingTask.promise;
+                if (!page) return;
+                const scale = 1;
+                const viewport = page.getViewport({scale});
 
-                // for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-                //     const page = await pdf.getPage(pageNum);
-                const page = await pdf.getPage(1);
-                    const scale = 1;
-                    const viewport = page.getViewport({ scale });
+                // const canvas = canvasRef.current;
+                const canvas = document.createElement("canvas");
+                canvas.width = viewport.width;
+                canvas.height = viewport.height;
 
-                    const canvas = document.createElement("canvas");
-                    canvas.width = viewport.width;
-                    canvas.height = viewport.height;
+                const context = canvas.getContext("2d");
 
-                    const context = canvas.getContext("2d");
-                    if (!context) return;
+                const renderContext = {
+                    canvasContext: context,
+                    viewport: viewport,
+                };
 
-                    const renderContext = {
-                        canvasContext: context,
-                        viewport: viewport,
-                    };
+                const container = containerRef.current as HTMLDivElement;
+                if (container.firstChild) {
+                    container.removeChild(container.firstChild);
+                }
+                container.appendChild(canvas);
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                await page.render(renderContext).promise;
 
-                    container.appendChild(canvas);
 
-                    await page.render(renderContext).promise;
-                // }
             } catch (error) {
                 console.error("Error loading PDF:", error);
             } finally {
                 setTimeout(() => {
-                    setLoading(false); // 로딩이 끝났음을 표시
-                }, 1000);
+                    setLoading(false);
+                }, 500);
             }
         };
-
         loadPDF();
-    }, []);
+    }, [currentPage]);
+
+
+    /* 다음 페이지 보는 함수*/
+    async function onHandleNextPage() {
+        setCurrentPage(currentPage + 1);
+        /*
+        * preview 와 추가적으로 기능 작업 필요 -> canvas 이미지화하여 주고 받기 등....
+        * */
+    }
+
+    /* 이전 페이지 보는 함수*/
+    async function onHandlePrevPage() {
+        setCurrentPage(currentPage - 1);
+        /*
+        * preview 와 추가적으로 기능 작업 필요 -> canvas 이미지화하여 주고 받기 등....
+        * */
+    }
+
 
     return (
         <>
             {loading && (
-                <div className="fixed top-0 left-0 w-screen h-screen bg-white opacity-100">
-                    <Loading fileName={file.name}/>
-                </div>
+                <Loading fileName={file.name}/>
             )}
-            <div>
-                <div ref={containerRef}></div>
-            </div>
+            <div ref={containerRef}></div>
+            {currentPage !== 1 && (
+                <button className="btn" onClick={onHandlePrevPage}>prev</button>
+            )}
+            {currentPage !== numPages && (
+                <button className="btn" onClick={onHandleNextPage}>next</button>
+            )}
         </>
     );
 };
